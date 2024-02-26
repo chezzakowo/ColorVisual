05e2497028714b10cf7aa25a3a9e1b629b28a5a7
Chez_Zak <129508826+chezzakowo@users.noreply.github.com>
2024-02-26 20:59:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,38 @@
 let box = document.querySelector("div");
-let input = document.getElementById("colorInput");
-let toggleSwitch = document.getElementById("toggleSwitch");
-let dynamicBackground = false;
+let input = document.querySelector("input");
+let toggleSwitch = document.getElementById("toggle-switch");
+
+toggleSwitch.addEventListener("change", () => {
+    if (toggleSwitch.checked) {
+        changeBackground(input.value);
+    } else {
+        resetBackground();
+    }
+});
 
 input.addEventListener("keyup", () => {
-    let value = input.value.trim();
-    
-    if (value === "") {
-        if (dynamicBackground) {
-            document.body.classList.remove("dynamic-background");
-            dynamicBackground = false;
-        }
-        box.style.backgroundImage = `url('your-background-image-url.jpg')`;
-    } else {
-        if (!dynamicBackground) {
-            document.body.classList.add("dynamic-background");
-            dynamicBackground = true;
-        }
-        if (/^#?([0-9a-fA-F]{3}|[0-9a-fA-F]{6})$/.test(value) || /^rgb\((\s*\d{1,3}\s*,){2}\s*\d{1,3}\s*\)$/.test(value)) {
-            box.style.animation = "colorChange 1s forwards";
-            box.style.background = value;
-            document.body.style.background = value;
-        } else {
-            box.style.animation = "none";
-            box.style.background = "transparent";
-            document.body.style.background = "transparent";
-            box.style.backgroundImage = `url('${value}')`;
-        }
+    if (toggleSwitch.checked) {
+        changeBackground(input.value);
     }
 });
 
-toggleSwitch.addEventListener("click", () => {
-    dynamicBackground = !dynamicBackground;
-    if (dynamicBackground) {
-        document.body.classList.add("dynamic-background");
-        input.dispatchEvent(new Event("keyup"));
+function changeBackground(value) {
+    if (isValidColor(value)) {
+        box.style.backgroundColor = value;
+        document.body.style.backgroundColor = value;
     } else {
-        document.body.classList.remove("dynamic-background");
+        box.style.backgroundImage = "url('https://cdn.discordapp.com/attachments/1044570469279481907/1211673823364251688/FB_IMG_1705191223026.jpg?ex=65ef0e2f&is=65dc992f&hm=fd6b92bb4f63956dca732a371e5b5434704b2defb59f6529423de14943be1135&')";
+        document.body.style.backgroundImage = "url('https://cdn.discordapp.com/attachments/1044570469279481907/1211673823364251688/FB_IMG_1705191223026.jpg?ex=65ef0e2f&is=65dc992f&hm=fd6b92bb4f63956dca732a371e5b5434704b2defb59f6529423de14943be1135&')";
     }
-});
+}
+
+function resetBackground() {
+    box.style.backgroundImage = "url('https://cdn.discordapp.com/attachments/1044570469279481907/1211673823364251688/FB_IMG_1705191223026.jpg?ex=65ef0e2f&is=65dc992f&hm=fd6b92bb4f63956dca732a371e5b5434704b2defb59f6529423de14943be1135&')";
+    document.body.style.backgroundImage = "url('https://cdn.discordapp.com/attachments/1044570469279481907/1211673823364251688/FB_IMG_1705191223026.jpg?ex=65ef0e2f&is=65dc992f&hm=fd6b92bb4f63956dca732a371e5b5434704b2defb59f6529423de14943be1135&')";
+}
+
+function isValidColor(color) {
+    // Regular expression to match hex, rgb, or rgba color values
+    let colorRegex = /^(#([0-9a-fA-F]{3}){1,2}|(rgb|rgba)\((\d{1,3}),(\d{1,3}),(\d{1,3})(,(\d*\.?\d+))?\))$/;
+    return colorRegex.test(color);
+}
